c1f34a11808c12867e5ee0a4fe25d6d5dc0e8a9a
Marco Jiménez Ureña <33840318+MarcoJimenez11@users.noreply.github.com>
2024-12-12 10:55:21 +0100

    Update Tuition.js
    Added recommended changes in class using methods: toLocaleDateString( ) and reduce( )

--- js/Tuition.js
+++ js/Tuition.js
@@ -21,16 +21,23 @@ export class Tuition extends NamedList {
     //Devuelve la fecha dada por parámetro en formato español (dd-mm-yyyy)
     getSpanishDate(date) {
         date = new Date(date);
-        var dd = date.getDate();
-        var mm = date.getMonth() + 1;   //Comienza en 0
-        var yyyy = date.getFullYear();
-        if (dd < 10) {
-            dd = '0' + dd;
-        }
-        if (mm < 10) {
-            mm = '0' + mm;
-        }
-        return dd + '-' + mm + '-' + yyyy;
+        // var dd = date.getDate();
+        // var mm = date.getMonth() + 1;   //Comienza en 0
+        // var yyyy = date.getFullYear();
+        // if (dd < 10) {
+        //     dd = '0' + dd;
+        // }
+        // if (mm < 10) {
+        //     mm = '0' + mm;
+        // }
+        // return dd + '-' + mm + '-' + yyyy;
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        };
+        var newDate = date.toLocaleDateString("es-ES", options);
+        return newDate;
     }
 
     setCancelDate(date) {
@@ -40,10 +47,12 @@ export class Tuition extends NamedList {
     //Calcula la media de las notas almacenadas en la matrícula
     getAverage() {
         var avg = 0;
-        this.elements.forEach(val => {
-            avg += parseInt(val);
-        });
-        avg /= this.elements.length;
+        // this.elements.forEach(val => {
+        //     avg += parseInt(val);
+        // });
+        // avg /= this.elements.length;
+        // return parseInt(avg);
+        avg = this.elements.reduce((sum, value) => { return sum + value; }, 0) / this.elements.length;
         return parseInt(avg);
     }
 
